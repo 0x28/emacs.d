75da2207fbf81222f6b10f1dc185f4d36feae037
0x28 <none>
2017-11-26 16:22:31 +0100

    Initial commit

---
+++ init.el
@@ -0,0 +1,17 @@
+;;; init.el -- test
+;;; Commentary:
+;;; Code:
+(require 'org)
+(org-babel-load-file
+ (expand-file-name "settings.org"
+		   user-emacs-directory))
+
+(provide 'init)
+
+;;; init.el ends here
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(org-block ((t (:background "gray9" :foreground "white smoke")))))
