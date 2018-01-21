95b022611d0f5d8b50375b133087c5626b19579a
0x28 <none>
2018-01-21 15:57:06 +0100

    Change whitespace-trailing font

--- init.el
+++ init.el
@@ -19,7 +19,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(org-block-background ((t (:background "gray6")))))
+ '(org-block-background ((t (:background "gray6"))))
+ '(whitespace-trailing ((t (:background "orange red" :foreground "black")))))
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
