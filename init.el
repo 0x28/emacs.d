54ff00c46603157b5d032d9ce4a8f7d2027d9496
0x28 <none>
2018-12-22 16:36:23 +0100

    Use color for arguments in help mode

--- init.el
+++ init.el
@@ -23,9 +23,10 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(help-argument-name ((t (:inherit (italic minibuffer-prompt)))))
  '(mode-line ((t (:box (:line-width -1 :color "black")))))
- '(org-block-begin-line ((t (:inherit org-meta-line :box -1))) t)
- '(org-block-end-line ((t (:inherit org-meta-line :box -1))) t)
+ '(org-block-begin-line ((t (:inherit org-meta-line :box -1))))
+ '(org-block-end-line ((t (:inherit org-meta-line :box -1))))
  '(whitespace-trailing ((t (:background "orange red" :foreground "black")))))
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
