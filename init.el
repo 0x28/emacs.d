39963e2b4f0972688460c0b0a7050f37bd1dabc8
0x28 <none>
2019-06-16 16:40:38 +0200

    Improve startup time by deactivating tool bar and scroll bar using custom variables

--- init.el
+++ init.el
@@ -33,4 +33,6 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(org-src-fontify-natively t))
+ '(org-src-fontify-natively t)
+ '(scroll-bar-mode nil)
+ '(tool-bar-mode nil))
