99a3cd6de8c5a5a24138572c23e0e64cc5ede05f
0x28 <none>
2019-07-17 21:30:14 +0200

    Show help for object at point in minibuffer when possible
    This is really useful for flymake-mode.

--- init.el
+++ init.el
@@ -33,6 +33,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(help-at-pt-display-when-idle t nil (help-at-pt))
  '(org-src-fontify-natively t)
  '(scroll-bar-mode nil)
  '(tool-bar-mode nil))
