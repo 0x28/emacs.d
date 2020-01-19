9e8c8f7b3e4127a803da2182d186bf5209808ceb
0x28 <none>
2020-01-19 21:49:34 +0100

    Change face color of help-argument-name

--- init.el
+++ init.el
@@ -20,7 +20,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(help-argument-name ((t (:inherit (italic minibuffer-prompt)))))
+ '(help-argument-name ((t (:inherit (italic font-lock-function-name-face)))))
  '(mode-line ((t (:overline "gainsboro" :background nil))))
  '(mode-line-inactive ((t (:overline "dim gray" :background nil))))
  '(org-block-begin-line ((t (:box -1))))
@@ -31,10 +31,10 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(help-at-pt-display-when-idle t nil (help-at-pt))
+ '(menu-bar-mode nil)
  '(org-src-fontify-natively t)
  '(scroll-bar-mode nil)
- '(tool-bar-mode nil)
- '(menu-bar-mode nil))
+ '(tool-bar-mode nil))
 
 (provide 'init)
 ;;; init.el ends here
