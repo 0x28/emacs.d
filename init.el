6589bc6aca981812d291115872ba19ad6d41e723
0x28 <none>
2020-04-13 22:35:21 +0200

    Move faces config to settings.org

--- init.el
+++ init.el
@@ -9,14 +9,6 @@
    (expand-file-name "settings.org"
                      user-emacs-directory)))
 
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(help-argument-name ((t (:inherit (italic font-lock-function-name-face)))))
- '(mode-line ((t (:overline "gainsboro" :background nil))))
- '(mode-line-inactive ((t (:overline "dim gray" :background nil)))))
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
