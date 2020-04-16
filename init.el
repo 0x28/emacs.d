cebe62d6eafca0be27f8ab523b79fb5a84d8039a
0x28 <none>
2020-04-16 10:05:28 +0200

    Move custom-set-variables config to settings.org

--- init.el
+++ init.el
@@ -9,16 +9,5 @@
    (expand-file-name "settings.org"
                      user-emacs-directory)))
 
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(help-at-pt-display-when-idle t nil (help-at-pt))
- '(menu-bar-mode nil)
- '(scroll-bar-mode nil)
- '(tool-bar-mode nil)
- '(indent-tabs-mode nil))
-
 (provide 'init)
 ;;; init.el ends here
