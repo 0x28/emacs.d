80b9ff7669c88c0991fe10e00b0ea07ebbe30871
0x28 <12886563+0x28@users.noreply.github.com>
2021-02-20 14:12:00 +0100

    Move default-frame-alist configurations to early-init.el

--- early-init.el
+++ early-init.el
@@ -1,1 +1,15 @@
 (setq package-quickstart t)
+
+;; disable scroll bar
+(add-to-list 'default-frame-alist '(vertical-scroll-bars . nil))
+;; disable menu bar
+(add-to-list 'default-frame-alist '(menu-bar-lines . 0))
+;; disable tool bar
+(add-to-list 'default-frame-alist '(tool-bar-lines . 0))
+;; start emacs maximized
+(add-to-list 'default-frame-alist '(fullscreen . maximized))
+;; use hack or consolas font
+(add-to-list 'default-frame-alist
+             (cons 'font
+                   (cond ((eq system-type 'gnu/linux) "Hack-13")
+                         ((eq system-type 'windows-nt) "Consolas-14"))))
